f21a6552f80f882f7258bf24380618fa2c2689c5
Blok! <windpourwow@gmail.com>
2022-12-22 14:58:20 +0100

    update progress
    added getSkins functions and fixed getInfo functions

--- nuevoBot.py
+++ nuevoBot.py
@@ -1,53 +1,70 @@
 from bs4 import BeautifulSoup
+import json
 import sys
 import requests
 
-def getScrappedInfo(shipURL):
-    resp = requests.get(shipURL).text
-    doc = BeautifulSoup(resp, "html.parser")
-    return doc
 
-def getShipSkins(shipURL):
+class AzurLaneTB:
+    def __init__(self, shipURL) -> None:
+        self.URL = shipURL
+        self.main = self.getScrappedInfo()
+        self.skins = self.getShipSkins()
+        self.skills = self.getSkills()
 
-    resp = requests.get(f'{shipURL}/Gallery').text
-    doc = BeautifulSoup(resp, "html.parser")
-    mainWindow = doc.find('section', class_='tabber__section').find_all(recursive=False)
-    [print(skin['data-title']) for skin in mainWindow]
-        #print(skin.find('div', class_='shipskin')) 
-    for skin in mainWindow:
-        print(parseSRCSET(skin.find('div', class_='shipskin-image').find('img')['srcset']))
+    def getScrappedInfo(self):
+        resp = requests.get(self.URL).text
+        doc = BeautifulSoup(resp, "html.parser")
+        return doc
 
-def parseSRCSET(srcset):
-    splitted = srcset.split(' ')
-    splitted =splitted[len(splitted)-2].replace('thumb/','')
-    fullRes = splitted.split('/')[:-1]
-    return '/'.join(fullRes)  if 'px-' in splitted else splitted
-    
-def getShipInfo(shipDoc):
-    mainCard = shipDoc.find('div', class_='ship-card-content')
-    shipFullName = mainCard.find('div', class_='card-headline').find('span').text
-    mainCardInfo = mainCard.find('div', class_='card-info').find('tbody')
-    infoTRs = mainCardInfo.find_all('tr')
-    skillsTable = shipDoc.find('table',class_='ship-skills wikitable')
-    skillsTR = skillsTable.find_all('tr')[1:]
+    def getShipSkins(self):
+        skinsDict = {"skins": []}
+        resp = requests.get(f'{self.URL}/Gallery').text
+        doc = BeautifulSoup(resp, "html.parser")
+        mainWindow = doc.find(
+            'section', class_='tabber__section').find_all(recursive=False)
+        for skin in mainWindow:
+            image = skin.find('div', class_='shipskin-image').find('img')
+            if image != None:
+                name = skin['data-title']
+                link = self.fixSource(src=image['srcset'].strip())
+                skinsDict["skins"].append({"name": name , "url": link})
+                # print(self.fixSource(src=skin.find('div', class_='shipskin-image').find('img')['srcset'].strip())+'\n')
 
-    for info in infoTRs[0:5]:
-        children = info.findChildren()
-        print(f'Category {children[0].text.strip()}')
-        print(children[-1].text.strip())
-    
-    print('Category Skills')
-    for skill in skillsTR:
-        if skill.find('b') != None:
-            print(skill.find_all('td')[1].find('b').text)
-            print(skill.find_all('td')[2].text)
+        return skinsDict
 
-    
-    return 'Done' 
+    def getInfo(self):
+        mainCard = self.main.find('div', class_='ship-card-content')
+        shipFullName = mainCard.find(
+            'div', class_='card-headline').find('span').text
+        mainCardInfo = mainCard.find('div', class_='card-info').find('tbody')
+        infoTRs = mainCardInfo.find_all('tr')
+
+        for info in infoTRs[0:5]:
+            children = info.findChildren()
+            print(f'Category {children[0].text.strip()}')
+            print(children[-1].text.strip() + '\n')
+
+        return 'Done'
+
+    def getSkills(self):
+        skillsTable = self.main.find('table', class_='ship-skills wikitable')
+        skillsTR = skillsTable.find_all('tr')[1:]
+        for skill in skillsTR:
+            if skill.find('b') != None:
+                print(self.fixSource(skill.find('img')['srcset']))
+                print(skill.find_all('td')[1].find('b').text)
+                print(skill.find_all('td')[2].text.strip() + '\n')
+
+    def fixSource(self, src):
+        splitted = src.split(' ')
+        splitted = splitted[len(splitted)-2].replace('thumb/', '')
+        fullRes = splitted.split('/')[:-1]
+        return '/'.join(fullRes) if 'px-' in splitted else splitted
 
 
-
-#Main function
+# Main function
 if __name__ == '__main__':
-    #scrappedDoc = getScrappedInfo(sys.argv[1])
-    getShipSkins(sys.argv[1])
+    # scrappedDoc = getScrappedInfo(sys.argv[1])
+    input = sys.argv[1]
+    tb = AzurLaneTB(shipURL=input)
+    print(json.dumps(tb.skins,indent=4))
