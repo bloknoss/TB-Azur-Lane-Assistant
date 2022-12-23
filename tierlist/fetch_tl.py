bb7a5d63abd94d9939967ebf77a6845675a2433c
Blok! <windpourwow@gmail.com>
2022-12-23 01:05:56 +0100

    tier list commit
    fetch tier list rankings from azur lane community tier list

---
+++ tierlist/fetch_tl.py
@@ -0,0 +1,45 @@
+import json
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+
+
+
+types = ["bb","cv","ca","cl","dd","ss","other"]
+tierList = {"BB":[],"CV":[],"CA":[],"CL":[],"DD":[],"SS":[],"OTHER":[]}
+print("Updating local database from https://slaimuda.github.io/ectl/#/home.\nThis could take up to a few minutes.\n")
+for type in types:
+    
+    session = HTMLSession()
+
+    resp = session.get(f'https://slaimuda.github.io/ectl/#/main/{type}')
+    resp.html.render()
+    soup = BeautifulSoup(resp.html.html, 'html.parser')
+    
+    tiers = soup.find('div', class_='tab-pane active').find_all('div', class_=None)
+    for tier in tiers:
+        if tier.find('h3') != None:
+            ships = tier.find_all('div', class_='col-4')
+            rank = tier.find('h3').text.strip()
+            currentRank = {"ships":[]}
+            for ship in ships:
+                additionalNotes = ship.find_all('img', class_=None)
+                name = ship.text
+                if additionalNotes != None:
+                   additionalNotes = [additionalNote["data-tip"].strip() for additionalNote in additionalNotes]
+                else:
+                    additionalNotes = None
+                
+                currentRank["ships"].append({"name" : name, "notes" : additionalNotes})
+            tierList[type.upper()].append({rank:currentRank})
+            print(f"Tier for {type.upper()}'s {rank} fetched succesfully.")
+    print()
+
+with open("tierList.json", "w") as fp:
+    json.dump(tierList,fp,indent=4)
+    
+print("\nUpdating has finished succesfully.\nSaved on tierList.json")
+# for tier in tiers:
+#    print("xds")
+#    ships = tier.find_all('col-4')
+#    for ship in ships:
+#        print(ship.text)
